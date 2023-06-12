78cf76b7c3dee7dbe8734bf31aec819a92241753
Lorenzo Rybu Buglione <lorenzorybuglione@gmail.com>
2023-06-12 14:26:27 -0300

    Basic Navigation, Header and Footer

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import logo from "./logo.png";
+import styles from "./Header.module.css";
+import HeaderLink from "components/HeaderLink";
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <Link to="./">
+        <img src={logo} alt="Logo do Cinetag" />
+      </Link>
+      <nav>
+        <HeaderLink url="./">Home</HeaderLink>
+        <HeaderLink url="./Favoritos">Favoritos</HeaderLink>
+      </nav>
+    </header>
+  );
+}
+export default Header;
