86bd358437b0d53831ab98ccfd9620219c5f2491
Lorenzo Rybu Buglione <lorenzorybuglione@gmail.com>
2023-06-15 11:25:28 -0300

    Dynamic Cards Listed

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,16 +3,23 @@ import Card from "components/Card";
 import Footer from "components/Footer";
 import Header from "components/Header";
 import Title from "components/Title";
+import videos from "json/db.json";
+
+import styles from "./Home.module.css";
 
 function Home() {
   return (
     <>
       <Header />
-      <Banner image={'home'}/>
+      <Banner image={"home"} />
       <Title>
         <h1>Um Lugar para guardar seus vídeos e filmes!</h1>
       </Title>
-      <Card id='1' title='Gato Bonifácio' image='https://github.com/LorenzoRybuBuglione.png' />
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return <Card {...video} key={video.id} />;
+        })}
+      </section>
       <Footer />
     </>
   );
