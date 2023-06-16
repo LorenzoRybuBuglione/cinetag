faf6b0123a8df41687ad8ca51b8134f1b01ea6b1
Lorenzo Rybu Buglione <lorenzorybuglione@gmail.com>
2023-06-16 12:04:53 -0300

    Context Favorite and Unfavorite

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,25 @@
+import { useFavoriteContext } from "contexts/Favorites";
 import styles from "./Card.module.css";
-import favIcon from "./favorite_outline.png";
+import notFavIcon from "./favorite_outline.png";
+import favIcon from "./favorite.png";
 
 function Card({ id, title, image }) {
+  const { favorite, addFavorite } = useFavoriteContext();
+  const isFavorite = favorite.some((fav) => fav.id === id);
+
   return (
-  <div className={styles.container}>
-    <img src={image} alt={title} className={styles.image} />
-    <h2>{title}</h2>
-    <img src={favIcon} alt="Favoritar filme" className={styles.favorite} />
-  </div>
+    <div className={styles.container}>
+      <img src={image} alt={title} className={styles.image} />
+      <h2>{title}</h2>
+      <img
+        src={isFavorite? favIcon : notFavIcon}
+        alt="Favoritar filme"
+        className={styles.favorite}
+        onClick={() => {
+          addFavorite({ id, title, image });
+        }}
+      />
+    </div>
   );
 }
 
